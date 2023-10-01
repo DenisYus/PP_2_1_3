e65e7f2520d0b50605a39ad532ca1363a44faa80
DenisYus <denis-yus3@mail.ru>
2023-10-01 21:15:30 +0300

    1

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -5,11 +5,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 
-public class Timer {
-
-    private Long nanoTime = System.nanoTime();
-
-    public Long getTime() {
-        return nanoTime;
+public class Dog extends Animal {
+    @Override
+    public String toString() {
+        return "Im a Dog";
     }
 }
